3563b7673bbf80ea4fa270d391da9f72212919da
Armando Santos <armando@well-typed.com>
2024-01-22 11:40:49 +0000

    Update Nixops deployment configuration (#9)
    - Add BlockPerf script that reports metrics to CF
    - Fixed firewall issue with nodes
    - Tweaked logging configuration for nodes
    - Update NixOS and Nixpkgs version
    - Update cardano-node version
    - Update AWS machine type used
    - Update scripts

--- dev-deployer-terraform/modules/multi-region/main.tf
+++ dev-deployer-terraform/modules/multi-region/main.tf
@@ -1,5 +1,5 @@
 variable "ami" {}
-variable "instance_type" { default = "t3a.2xlarge" }
+variable "instance_type" { default = "m5.xlarge" }
 
 # If universal access is needed from another machine this is the place to add it.
 #                                        ---v dev-deployer machine
@@ -18,10 +18,10 @@ resource "aws_security_group" "allow_deployer_sg" {
   name        = "allow_deployer_sg"
 
   ingress {
-    description = "allow all from deployer"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "allow ssh from deployer"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = var.allowed-to-access
   }
 }
@@ -30,18 +30,11 @@ resource "aws_security_group" "allow_all_sg" {
   name    = "allow_all_sg"
 
   ingress {
-    description = "allow all hosts to access any port greater than 1023 (IPv4)"
+    description = "allow all hosts to access any port greater than 1023 (IPv4 and IPv6)"
     from_port   = 1024
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "allow all hosts to access any port greater than 1023 (IPv6)"
-    from_port        = 1024
-    to_port          = 65535
-    protocol         = "tcp"
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -51,13 +44,26 @@ resource "aws_security_group" "allow_all_sg" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "allow_icmp_sg" {
+  name        = "allow_icmp_sg"
+
+  ingress {
+    description = "allow all ICMP (IPv4)"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    description      = ""
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
+  ingress {
+    description = "allow all ICMP (IPv6)"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmpv6"
     ipv6_cidr_blocks = ["::/0"]
   }
 }
@@ -77,7 +83,8 @@ resource "aws_instance" "cad-2694-node" {
     volume_type = "standard"
   }
   security_groups = [
-    aws_security_group.allow_deployer_sg.name,
-    aws_security_group.allow_all_sg.name
+    aws_security_group.allow_all_sg.name,
+    aws_security_group.allow_icmp_sg.name,
+    aws_security_group.allow_deployer_sg.name
   ]
 }
